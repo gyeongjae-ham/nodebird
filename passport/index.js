6698177df4cb08194fb0e7d79d96466bb43d2819
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-20 12:21:20 +0900

    Added follow feature.

--- passport/index.js
+++ passport/index.js
@@ -9,8 +9,19 @@ module.exports = () => {
     });
 
     passport.deserializeUser((id, done) => {
-        User.findOne({ where: { id } })
-            .then(user => done(null, user)) // req.user, req.isAutherticated()
+        User.findOne({
+            where: { id },
+            include: [{
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followers',
+            }, {
+                model: User,
+                attributes: ['id', 'nick'],
+                as: 'Followings',
+            }],
+        })
+            .then(user => done(null, user))
             .catch(err => done(err));
     });
 
