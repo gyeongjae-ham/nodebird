ed5c222c4f34f7dcde69768b4d6248ef033c9d88
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-28 16:25:47 +0900

    Seperated user.js controllers.

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,8 @@ exports.addFollowing = async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.user.id } });
         if (user) {
-            await Follow.destroy({ where: { id: req.user.id } });
-            res.send('sucess');
+            await user.addFollowing(parseInt(req.params.id, 10));
+            res.send('success');
         } else {
             res.status(404).send('no user');
         }
